7e2b45d0435918c90d941f962fe5d6e107965b61
Anaz Zubair <anazzubair@yahoo.com>
2018-01-14 11:07:47 +0400

    Restructuring

---
+++ TestIot/Modules/HelloModule.cs
@@ -0,0 +1,35 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestIot.Utilities;
+
+namespace TestIot.Modules
+{
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = parameters => "Hello World";
+
+            Post["/httpConnector"] = _ =>
+            {
+                dynamic model = Context.ToDynamic();
+                var data = model[0].payload.data;
+                //if (((string)model[0].payload.format).Contains("coffee"))
+                //{
+                //    Console.WriteLine($"WaterLevel: {data.Water_Level}  No. of cups: ${data.No_of_cups_brewed}");
+                //}
+                //else
+                //{
+                //    if (((string)model[0].payload.format).Contains("ALERT"))
+                //        Console.WriteLine(model);
+                //}
+                Console.WriteLine(model);
+                return Nancy.HttpStatusCode.OK;
+            };
+        }
+    }
+}
---
+++ TestIot/Utilities/DynamicModelBinder.cs
@@ -0,0 +1,26 @@
+﻿using Nancy;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIot.Utilities
+{
+    public static class DynamicModelBinder
+    {
+        public static dynamic ToDynamic(this NancyContext context)
+        {
+            var serializer = new JsonSerializer();
+            using (var sr = new StreamReader(context.Request.Body))
+            {
+                using (var jsonTextReader = new JsonTextReader(sr))
+                {
+                    return serializer.Deserialize(jsonTextReader);
+                }
+            }
+        }
+    }
+}
